a2c39ea617d8e538989d3c71187e3c94bc872988
847920524 <32091401+847920524@users.noreply.github.com>
2017-10-19 18:19:24 +0800

    最新

--- www/views/category/category-list.controller.js
+++ www/views/category/category-list.controller.js
@@ -1,7 +1,13 @@
-(function () {
+﻿(function () {
   'use strict'
   angular.module('starter.controllers')
-    .controller('CategoryListCtrl',['$scope', function ($scope) {
+    .controller('CategoryListCtrl',['$scope','$ionicActionSheet','$ionicHistory','CategoryService','productService','popupService', function ($scope,$ionicActionSheet,$ionicHistory,CategoryService,productService,popupService) {
+      $scope.$on('$stateChangeSuccess',function (event,toState,toParams,fromState,fromParams) {
+        $scope.showInfo='无小分类进入大分类';
+        if(fromState.name=='app.product-list'){
+          $scope.showInfo='全部商品';
+        }
+      });
       $scope.categories = [
         {
           ID: 1,
@@ -9,22 +15,22 @@
           Children: [
             {
               ID: 11,
-              Name: '豆浆',
+              Name: '油条',
               Children: []
             },
             {
               ID: 12,
-              Name: '馒头',
+              Name: '面',
               Children: []
             },
             {
               ID: 13,
-              Name: '包子',
+              Name: '粉',
               Children: []
             },
             {
               ID: 14,
-              Name: '面线糊',
+              Name: '细粉',
               Children: []
             }
           ]
@@ -40,7 +46,7 @@
             },
             {
               ID: 22,
-              Name: '猪肉',
+              Name: '鸡肉',
               Children: []
             },
             {
@@ -50,7 +56,7 @@
             },
             {
               ID: 24,
-              Name: '鸡肉',
+              Name: '汉堡',
               Children: []
             }
           ]
@@ -61,7 +67,7 @@
           Children: [
             {
               ID: 31,
-              Name: '茶叶蛋',
+              Name: '面包',
               Children: []
             },
             {
@@ -71,12 +77,12 @@
             },
             {
               ID: 33,
-              Name: '排骨',
+              Name: '粗粉',
               Children: []
             },
             {
               ID: 34,
-              Name: '腊汁肉',
+              Name: '羊肉',
               Children: []
             }
           ]
@@ -97,18 +103,19 @@
             },
             {
               ID: 43,
-              Name: '苹果汁',
+              Name: '芬达',
               Children: []
             },
             {
               ID: 44,
-              Name: '芒果汁',
+              Name: '雪碧',
               Children: []
             }
           ]
         },
       ];
       $scope.activeCategory = {};
+      $scope.activeSubCategory = {};
       if ($scope.categories.length > 0) {
         $scope.activeCategory = $scope.categories[0];
       }
@@ -117,6 +124,44 @@
           $scope.activeCategory = $scope.categories[index];
         }
       };
+      $scope.selectSubCategory = function (data) {
+        console.log(data);
+        $scope.activeSubCategory = data;
+        $ionicHistory.goBack();
+      };
+      $scope.showActionSheet = function () {
+        $ionicActionSheet.show({
+          buttons:[
+            {
+              text:'<b>新增小分类</b>'
+            },
+            {
+              text:'编辑分类'
+            }
+          ],
+         cancelText:'取消',
+          buttonClicked:function (index) {
+            switch(index){
+              case 0:
+                    $scope.gotoCategoryAdd();
+                    break;
+              case 1:
+
+                    break;
+            }
+          },
+          titleText:'更多操作'
+        });
+      };
+      $scope.gotoCategoryAdd=function () {
+        location.href='#/app/category-add/' +$scope.activeCategory.ID + '/' +$scope.activeCategory.Name;
+      }
+      $scope.$watch('activeSubCategory',function (newValue,oldValue) {
+        if(newValue.ID){
+          CategoryService.updateCategory($scope.activeSubCategory);
+        }
+      })
     }
     ]);
 })();
+
---
+++ www/views/product/product-list.controller.js
@@ -0,0 +1,116 @@
+﻿(function () {
+  'use strict';
+  angular.module('starter.controllers')
+    .controller('ProductLoading',['$scope','$ionicLoading','$filter','$timeout',function ($scope,$ionicLoading,$filter,$timeout) {
+      $scope.products=[];
+      $scope.sourceProducts = [];
+      $scope.searchMV={
+        content:''
+      };
+      $scope.hasMore=true;
+      var isLoading = false;
+      var pageIndex = 1;
+      $scope.$on('$ionicView.enter',function () {
+       $ionicLoading.show({
+         template:'<ion-spinner icon="ios" class="spinner-list"></ion-spinner>数据加载中，请稍后......'
+       });
+        $scope.doRefresh();
+      });
+      $scope.getByNaem=function () {
+        $scope.products = $filter('filter')($scope.sourceProducts,{
+          Name:$scope.searchMV.content
+        });
+      };
+      $scope.doRefresh = function () {
+        pageIndex=1;
+        $scope.hasMore=true;
+        $scope.products=[];
+        loadData();
+      };
+      $scope.loadMore=function () {
+        pageIndex++;
+        loadData();
+      };
+      function  loadData() {
+        if (isLoading == true) {
+          return;
+        }
+        isLoading = true;
+        $timeout(function () {
+          var list = [
+            {
+              ID: 1
+              , Images: ['views/product/images/1.jpg']
+              , Name: 'iphone4'
+              , Price: '1111'
+              , Store: 1111
+              , Barcode: '111'
+            }
+            , {
+              ID: 2
+              , Images: ['views/product/images/2.jpg']
+              , Name: 'iphone4s'
+              , Price: '2222'
+              , Store: 222              , Barcode: '222'
+            }
+            , {
+              ID: 3
+              , Images: ['views/product/images/3.jpg']
+              , Name: 'iphone5'
+              , Price: '3333'
+              , Store: 33
+              , Barcode: '333333'
+            }
+            , {
+              ID: 4
+              , Images: ['views/product/images/4.jpg']
+              , Name: 'iphone'
+              , Price: '4444'
+              , Store: 44
+              , Barcode: '444444'
+            }
+            , {
+              ID: 5
+              , Images: ['views/product/images/5.jpg']
+              , Name: 'iphone5s'
+              , Price: '555'
+              , Store: 55
+              , Barcode: '5555'
+            }
+            , {
+              ID: 6
+              , Images: ['views/product/images/6.jpg']
+              , Name: 'iphone5e'
+              , Price: '6666'
+              , Store: 66
+              , Barcode: '6666'
+            }
+            , {
+              ID: 7
+              , Images: ['views/product/images/7.jpg']
+              , Name: 'iphone6'
+              , Price: '7777'
+              , Store: 77
+              , Barcode: '7777'
+            }
+            , {
+              ID: 7
+              , Images: ['views/product/images/8.jpg']
+              , Name: 'iphone7'
+              , Price: '8888'
+              , Store: 88              , Barcode: '8888'
+            }
+          ];
+          $scope.products=$scope.products.concat(list);
+          $scope.sourceProducts=angular.copy($scope.products);
+          $scope.$broadcast('scroll.refreshComplete');
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+          $ionicLoading.hide();
+          isLoading=false;
+          if(pageIndex==3){
+            $scope.hasMore=false;
+          }
+        }, 3000);
+      }
+    }]);
+})();
